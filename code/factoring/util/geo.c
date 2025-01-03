c590c3c3ca8af5267ec33975cc31c5c252ef9e49
Kevin Kerr <kkerr@triplelift.com>
2025-01-02 20:50:42 -0500

    sieving

--- code/factoring/util/geo.c
+++ code/factoring/util/geo.c
@@ -1,5 +1,22 @@
 #include "../factor.h"
 
+//TODO: revisit this
+ulong getStart(ulong n) {
+    ulong j = 1;
+    ulong bits = 1;
+    while (j <n && bits<64) {
+        j*=2;
+        bits++;
+    }
+    bits=bits/2;
+    j = 1;
+    while (bits >= 1) {
+        j*=2;
+        bits--;
+    }
+    return j;
+}
+
 ulong isSqr(ulong n) {
     ulong j = getStart(n);
     ulong l = 2;
