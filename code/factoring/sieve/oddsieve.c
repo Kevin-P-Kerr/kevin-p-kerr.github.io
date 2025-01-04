af59354dc04a8d29b352f5bf04baf2782f31ab13
Kevin Kerr <kkerr@triplelift.com>
2025-01-04 16:08:20 -0500

    getting closer

--- code/factoring/sieve/oddsieve.c
+++ code/factoring/sieve/oddsieve.c
@@ -6,8 +6,9 @@
 #include <stdio.h>
 
 prime_record *oddSieve(ulong n) {
-    // calculate the len of the bit array
-    ulong len = (n/32)+1;
+    ulong t = (n/2)+1; // the number of entries in the array
+    // calculate the len of the bit array in ulongs
+    ulong len = (t/64)+1;
     // initialize the bit array
     ulong *a = malloc(sizeof(ulong)*len);
     memset(a,0,sizeof(ulong)*len);
@@ -19,23 +20,27 @@ prime_record *oddSieve(ulong n) {
     ulong p = 0; // the current prime with which we mark
     while(p<=limit) {
         // find the next prime
-        for(;j<len;j++) {
-            if(!(isSet(j,a))) {
+        for(;j<t;j++) {
+            if (!isSet(j,a)) {
                 p = (2*j)+3;
                 break;
             }
         }
-        // mark
-        for(ulong i=j+p;i<n;i+=p) {
+        // mark with the prime
+        for(ulong i=j+p;i<t;i+=p) {
+            fprintf(stdout,"mark!\n");
             mark(i,a);
         }
+        // increment j to begin finding the next prime
+        j++;
     }
     // gather
     prime_record *anchor = malloc(sizeof(prime_record));
     prime_record *pr = anchor;
     pr->next = 0;
-    for(ulong i=0;i<len;i++) {
+    for(ulong i=0;i<t;i++) {
         if(!isSet(i,a)) {
+            fprintf(stdout,"hit!\n");
             pr->value = (2*i)+3;
             prime_record *nxt = malloc(sizeof(prime_record));
             nxt->next = 0;
